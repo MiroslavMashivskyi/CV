1275b48669e075046eee7e0fcf27a3b21af3a54a
MiroslavMashivskyi <62468496+MiroslavMashivskyi@users.noreply.github.com>
2020-03-25 13:23:46 +0200

    Updates

---
+++ precache-manifest.47fa2cbcce108053cd5f10e8dbbc0e34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba697012bc49e398e0296f6da49fa5a0",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "a5eda00b269b267ce65a",
+    "url": "/cv/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d8ca3aec3a0e1491d614",
+    "url": "/cv/static/js/2.a144b7c4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.a144b7c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5eda00b269b267ce65a",
+    "url": "/cv/static/js/main.55d7626c.chunk.js"
+  },
+  {
+    "revision": "60dcb580d665ad958b7c",
+    "url": "/cv/static/js/runtime-main.d59e7fa1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv/static/media/logo.5d5d9eef.svg"
+  }
+]);
